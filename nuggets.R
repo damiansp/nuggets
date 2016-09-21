dd11b91c414616f058a12de868a73781db41c85e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-21 16:22:50 -0400

    Added a few variables to handle temporal autocorrelation; saved 2 cleaned files one with probable outliers removed, one with them left in

--- nuggets.R
+++ nuggets.R
@@ -1,24 +1,31 @@
+#============================================================#
+#                                                            #
+# General exploration of the data and preprocessing          #
+#                                                            #
+# @author Damian Satterthwaite-Phillips <damiansp@gmail.com> #
+# @version 21 Sep 2016                                       #
+#                                                            #
+#============================================================#
+
 # Start with a clean workspspace:
 rm(list = ls())
 
 # Library imports
 #install.packages('sm', dependcies = T)     # etc, if not installed
 library(sm)
-library(mgcv) # for gam()
 
-# Preprocessing steps
-# Changed original file to read only (> chmod a-w)
-# Made copy, 'data.csv' to edit
-
-# Filled blanks with NA 
-                                        # moved 'revenue' (response var) to first field to be consistent with R
-# commands, e.g. lm(y ~)
+# Preprocessing prior to this:
+#   Changed original file to read only (> chmod a-w)
+#   Made copy, 'data.csv' to edit
+#   Filled blanks with NA 
+#   moved 'revenue' (response var) to first field to be consistent with R
+#   commands, e.g. lm(y ~)
 
 data <- read.csv('~/Desktop/cbtnuggetsbusinessanalyticsproject/data.csv')
 dim(data) # 117 records, 42 vars
-summary(data)
+# summary(data)
 
-                                        # Eyeball data visually, 9 variables at a time, to check for potential
+# Eyeball data visually, 9 variables at a time, to check for potential
 # outliers, data distributions, etc.
 par(mfrow = c(3, 3))
 for (i in 1:dim(data)[2]) {
@@ -33,7 +40,6 @@ for (i in 1:dim(data)[2]) {
   if (class(data[, i]) != 'factor') {
     hist(data[, i], main = names(data)[i])
     n <- n + 1
-    print(n)
   }
   
   if (n %% 9 == 0 ) { pause() }
@@ -41,7 +47,7 @@ for (i in 1:dim(data)[2]) {
 
 
 # Observations:
-# These are time series data, where time is clearly an important element
+# These are time series, and time is clearly an important element
 # Each record is a week's data; ordered temporally (top dates = most recent)
 
 # Binary variables (change to categorical vars):
@@ -76,6 +82,7 @@ data$highseason <- as.factor(data$highseason)
 data$holiday <- as.factor(data$holiday)
 data$autoenroll <- as.factor(data$autoenroll)
 data$socialperson <- as.factor(data$socialperson)
+data$promoteposts <- as.factor(data$promoteposts)
 data$learner <- as.factor(data$learner)
 
 
@@ -83,20 +90,98 @@ data$learner <- as.factor(data$learner)
 data$Week <- as.Date(data$Week, format = '%m/%d/%y')
 
 
+
+# Explore temporal correlation:
+
 # Because revenue is a time series, explore temporal effects further
-revenue.ts <- as.ts(data$revenue, start = '2014-02-02', frequency = 52)
 # Check for temporal autocorrelation in revenue values
-acf(data$revenue)
+acf(data$revenue) # yes signif for about 5 weeks lag
 # check for autocorrelation in variance
 acf((data$revenue - mean(data$revenue))^2) # slight at best
-plot(revenue.ts)
-abline(v = c(52, 104), col = 2) # divisions between years
+plot(data$revenue, type = 'l')
+abline(v = c(52, 104), col = 2) # divisions every 52 weeks (~1 yr)
+# With just over 2 years of data, it is not very clear, but there may be 
+# a seasonal effect: lower revenue in summer, higher near year ends
 
+# How well does differencing remove autocorrelation:
 plot(diff(data$revenue), type = 'l')
+abline(h = 0, col = 'grey')
 acf(diff(data$revenue))
+# pretty well, but note that the differenced distribution has fat tails
 hist(diff(data$revenue))
 shapiro.test(diff(data$revenue))
 
+# Add some potentially useful variables to deal with temporal AC
+data$diff1 <- c(NA, diff(data$revenue))
+data$diff2 <- c(rep(NA, 2), diff(data$revenue, difference = 2))
+data$diff3 <- c(rep(NA, 3), diff(data$revenue, difference = 3))
+data$diff4 <- c(rep(NA, 4), diff(data$revenue, difference = 4))
+
+# Make sure all weeks are evenly spaced (no gaps)
+diff(data$Week)
+# Appx 52 weeks per year; create new variable telling which week of the year 
+# it is (e.g., so first week of Mar 2015 can be grouped with first week of 
+# Mar 2016 to check for seasonal effects)
+# NOTE: data begin on Feb 2
+weekOfYear <- (data$weekno + 4) %% 52
+weekOfYear[weekOfYear == 0] <- 52
+data$weekOfYear <- weekOfYear
+# Verify correct alignment
+data[47:49, c('Week', 'weekOfYear')]
+data[52 + 47:49, c('Week', 'weekOfYear')]
+
+# Visualize
+plot(data$revenue, type = 'l')
+lines(80000 + 100 * weekOfYear, col = 4)
+abline(v = which(weekOfYear == 1), col = 2) # firs week of each yr
+
+# Store cleaned data
+write.csv(data, '~/Desktop/cbtnuggetsbusinessanalyticsproject/dataClean.csv')
+
+# For "outliers" that may be legitimate data (i.e., not input error), we will 
+# want to test models both with the data (assuming that they are correct), and 
+# with them removed (assuming they are errors), to compare which performs 
+# better.
+# Replace potential outliers with NA and save as a separate file
+
+# Potential outliers were in:
+# displayimp, displaylag, brandpaid, socialpostimp, Fblikes*, Fbengage* 
+# *possibly just very skewed data
+hist(data$displayimp)
+hist(data$displayimp[data$displayimp < 1e+08])
+data$displayimp[data$displayimp > 1e+08] <- NA
+
+hist(data$displaylag)
+hist(data$displaylag[data$displaylag < 1e+08])
+data$displaylag[data$displaylag > 1e+08] <- NA
+
+hist(data$brandpaid)
+hist(data$brandpaid[data$brandpaid < 150000])
+data$brandpaid[data$brandpaid > 150000] <- NA
+
+hist(data$socialpostimp)
+hist(data$socialpostimp[data$socialpostimp < 5e+6])
+data$socialpostimp[data$socialpostimp > 5e+6] <- NA
+
+# Given the distributions, the following are more likely to be real extreme 
+# values, and not input errors.  Leave alone for now.
+hist(data$Fblikes)
+plot(data$Fblikes)
+hist(data$Fbengage)
+plot(data$Fbengage)
+
+write.csv(
+  data, 
+  '~/Desktop/cbtnuggetsbusinessanalyticsproject/dataCleanNoOutlier.csv')
+
+
+
+
+
+
+
+
+# Further Exploration
 
 # Explore relationships between predictors and response
 par(mfrow = c(3, 3))
@@ -107,108 +192,6 @@ for (i in 2:dim(data)[2]) {
   if ((i - 1) %% 9 == 0 ) { pause() }
 }
 
-# Potentially significant nonlinear trends in:
+# Potentially significant nonlinear trends in, most of which can be
+# approximated  with a quadratic term:
 # brandpaid, fbposts, Twpostimp, twengage, twengagelag, blogsignup
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Explore with LM
-dim(data[complete.cases(data), ])
-rev.lm <- lm(revenue ~ . + 
-             c(NA, diff(data$revenue)) + 
-             c(rep(NA, 2), diff(data$revenue, difference = 2)) +
-             c(rep(NA, 3), diff(data$revenue, difference = 3)) +
-             c(rep(NA, 4), diff(data$revenue, difference = 4)) +
-             c(rep(NA, 5), diff(data$revenue, difference = 5)), 
-             data = data)
-rev.lm <- update(rev.lm, .~. - weekno - Fbpostimplag - Twpostimp - Fbpageimp)
-rev.lm <- update(rev.lm, .~. - emailopen)
-rev.lm <- update(rev.lm, .~. - twposts - blogvisits)
-rev.lm <- update(rev.lm, .~. - promoteposts)
-rev.lm <- update(rev.lm, .~. - holiday - nonbrandpaid - youtube - blogrefernew)
-rev.lm <- update(rev.lm, .~. - displayimp)
-rev.lm <- update(rev.lm, .~. - Fblikes - blognewusers)
-rev.lm <- update(rev.lm, .~. - emailclick - blogsignup)
-rev.lm <- update(rev.lm, .~. - socialpostimp - twengage - Fbpostimp)
-rev.lm <- update(rev.lm, .~. - displaylag - blogrefer)
-rev.lm <- update(rev.lm, .~. - learner - organicnew)
-rev.lm <- update(rev.lm, .~. - socialimp)
-rev.lm <- update(rev.lm, .~. - brandpaid)
-rev.lm <- update(rev.lm, .~. - socialimplag)
-rev.lm <- update(rev.lm, .~. - emailsent)
-rev.lm <- step(rev.lm, direction = 'both')
-summary(rev.lm)
-
-# Explore with GAM
-rev.gam <- gam(
-  revenue ~ Week + highseason + brandclicks + nonbrandclicks + youtubelag +
-  socialpostlag + s(fbposts) + Fbengage + fbengagelag + TWimpLag +
-  s(twengagelag) +
-  autoenroll + socialperson + 
-  c(NA, diff(data$revenue)) + 
-  c(rep(NA, 2), diff(data$revenue, difference = 2)) + 
-  c(rep(NA, 3), diff(data$revenue, difference = 3)) + 
-  c(rep(NA, 4), diff(data$revenue, difference = 4)) + 
-  c(rep(NA, 5), diff(data$revenue, difference = 5)) + 
-  s(brandpaid) + s(blogsignup) + s(fbposts) + s(Twpostimp) + s(twengage), 
-  data = data)
-summary(rev.gam)								# GCV = 5.1917e+07
-anova(rev.gam)
-
-#rev.gam1 <- update(rev.gam, . ~ . - highseason); summary(rev.gam1)	#GCV = 5.0332e+07
-#rev.gam1 <- update(rev.gam, . ~ . - Fbengage); summary(rev.gam1)	#GCV = 4.9177e+07
-rev.gam1 <- update(rev.gam, . ~ . - fbengagelag); summary(rev.gam1) #GCV = 4.9085e+07
-anova(rev.gam1)
-
-#rev.gam2 <- update(rev.gam1, . ~ . - highseason); summary(rev.gam2) #GCV = 4.7623e+07
-rev.gam2 <- update(rev.gam1, . ~ . - Fbengage); summary(rev.gam2)    #GCV = 4.6561e+07
-anova(rev.gam2)
-
-rev.gam3 <- update(rev.gam2, . ~ . - highseason); summary(rev.gam3) #GCV = 4.5078e+07
-anova(rev.gam3)
-
-#rev.gam4 <- update(rev.gam3, . ~ . - autoenroll); summary(rev.gam4) #GCV = 4.5143e+07
-#rev.gam4 <- update(rev.gam3, . ~ . - 
-#                   c(rep(NA, 5), diff(data$revenue, difference = 5))); summary(rev.gam4) 													#GCV = 4.5078e+07
-#rev.gam4 <- update(rev.gam3, . ~ . - s(twengagelag)); summary(rev.gam4) #
-
-par(mfrow = c(2, 3))
-plot(rev.gam3)
-
-# Consider nonlinear terms for 
-# fbposts, brandpaid, Twpostimp and twengage
-
-# NEXT:
-# Switch to python and sklearn:  Hold out a test set.
-# Use lasso to thin out the set of potential predictors
-
