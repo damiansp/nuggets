01e4ec66c410fbd334c7211f7e7d6750f19222fb
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-30 23:30:19 -0400

    added README, renamed .rmd files to lose the initial numbering; changed knit.R to reflect renamed files

--- revenue/knit.R
+++ revenue/knit.R
@@ -3,21 +3,23 @@
 library(knitr)
 
 knit2html(
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/1_nuggetsPreprocess.rmd',
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/1_nuggetsPreprocess.html')
+'~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/nuggetsPreprocess.rmd',
+'~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/1_nuggetsPreprocess.html'
+)
 
 knit2html(
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/2_nuggetAnalysis1.rmd',
+  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/nuggetAnalysis1.rmd',
   '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/2_nuggetAnalysis1.html')
           
 knit2html(
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/3_nuggetAnalysis2.rmd',
+  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/nuggetAnalysis2.rmd',
   '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/3_nuggetAnalysis2.html')          
   
 knit2html(
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/4_nuggetAnalysis3.rmd',
+  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/nuggetAnalysis3.rmd',
   '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/4_nuggetAnalysis3.html')          
   
 knit2html(
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/5_nuggetTreeAnalysis.rmd',
-  '~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/5_nuggetTreeAnalysis.html')          
+'~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/nuggetTreeAnalysis.rmd',
+'~/Desktop/cbtnuggetsbusinessanalyticsproject/revenue/5_nuggetTreeAnalysis.html'
+)          
